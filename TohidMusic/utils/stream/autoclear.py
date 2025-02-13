4eed7a7c257fd4042fcb6134155a78d41e8bfce5
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-02-13 15:33:22 +0530

    TohidMusic

--- TohidMusic/utils/stream/autoclear.py
+++ TohidMusic/utils/stream/autoclear.py
@@ -1,1 +1,38 @@
+# Copyright (C) 2025 by Tohid_Help @ Github, < https://github.com/Tohidkhan6332 >
+# Subscribe On YT < Tohidkhan_6332 >. All rights reserved. © Tohid © Mr Tohid.
 
+"""
+Tohidkhan6332 is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Tohid <https://github.com/Tohidkhan6332>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+import os
+
+from config import autoclean
+
+
+async def auto_clean(popped):
+    async def _auto_clean(popped_item):
+        try:
+            rem = popped_item["file"]
+            autoclean.remove(rem)
+            count = autoclean.count(rem)
+            if count == 0:
+                if "vid_" not in rem and "live_" not in rem and "index_" not in rem:
+                    try:
+                        os.remove(rem)
+                    except:
+                        pass
+        except:
+            pass
+
+    if isinstance(popped, dict):
+        await _auto_clean(popped)
+    elif isinstance(popped, list):
+        for pop in popped:
+            await _auto_clean(pop)
+    else:
+        raise ValueError("Expected popped to be a dict or list.")
