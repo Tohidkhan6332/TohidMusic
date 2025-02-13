9c6549ae6bb3af12f21b7cb9cda39c07bf1b1cc7
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-02-13 14:56:22 +0530

    TohidMusic

---
+++ TohidMusic/core/mongo.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2025 by Tohid_Help @ Github, < https://github.com/Tohidkhan6332 >
+# Subscribe On YT < Tohidkhan_6332 >. All rights reserved. © Tohid © Mr Tohid.
+
+"""
+Tohidkhan6332 is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Tohid <https://github.com/Tohidkhan6332>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import MONGO_DB_URI
+
+from ..logging import LOGGER
+
+LOGGER(__name__).info("Connecting to your Mongo Database...")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Tohid
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except:
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    exit()
+
+## Database For Broadcast Subscription By Team Tohid
+MONGODB_CLI = AsyncIOMotorClient(MONGO_DB_URI)
+db = MONGODB_CLI["subscriptions"]
