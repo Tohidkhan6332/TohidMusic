2f86f10dc38cd8a3333828b2a66f4c46b6693efc
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-02-13 15:23:03 +0530

    TohidMusic

---
+++ TohidMusic/plugins/tools/ping.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2025 by Tohid_Help @ Github, < https://github.com/Tohidkhan6332 >
+# Subscribe On YT < Tohidkhan_6332 >. All rights reserved. © Tohid © Mr Tohid.
+
+"""
+Tohidkhan6332 is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Tohid <https://github.com/Tohidkhan6332>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+from strings import get_command
+from TohidMusic import app
+from TohidMusic.core.call import Tohid
+from TohidMusic.utils import bot_sys_stats
+from TohidMusic.utils.decorators.language import language
+
+### Commands
+PING_COMMAND = get_command("PING_COMMAND")
+
+
+@app.on_message(filters.command(PING_COMMAND) & filters.group & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
+    start = datetime.now()
+    pytgping = await Tohid.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping)
+    )
