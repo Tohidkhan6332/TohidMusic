b9e172319042bcb6799a89cd5a2fc793b8a3f87c
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-02-13 14:58:43 +0530

    TohidMusic

--- TohidMusic/platforms/__init__.py
+++ TohidMusic/platforms/__init__.py
@@ -1,1 +1,19 @@
+# Copyright (C) 2025 by Tohid_Help @ Github, < https://github.com/Tohidkhan6332 >
+# Subscribe On YT < Tohidkhan_6332 >. All rights reserved. © Tohid © Mr Tohid.
 
+"""
+Tohidkhan6332 is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Tohid <https://github.com/Tohidkhan6332>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
