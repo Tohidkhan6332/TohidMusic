b0b0c7300535ab35ae4cdef1b4ab1dcc7e2d3ce4
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-02-13 15:31:19 +0530

    TohidMusic

--- TohidMusic/utils/inline/__init__.py
+++ TohidMusic/utils/inline/__init__.py
@@ -1,1 +1,19 @@
+# Copyright (C) 2025 by Tohid_Help @ Github, < https://github.com/Tohidkhan6332 >
+# Subscribe On YT < Tohidkhan_6332 >. All rights reserved. © Tohid © Mr Tohid.
 
+"""
+Tohidkhan6332 is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Tohid <https://github.com/Tohidkhan6332>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
+from .help import *
+from .play import *
+from .playlist import *
+from .queue import *
+from .settings import *
+from .song import *
+from .start import *
---
+++ TohidMusic/utils/inline/song.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2025 by Tohid_Help @ Github, < https://github.com/Tohidkhan6332 >
+# Subscribe On YT < Tohidkhan_6332 >. All rights reserved. © Tohid © Mr Tohid.
+
+"""
+Tohidkhan6332 is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Tohid <https://github.com/Tohidkhan6332>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+import config
+from pyrogram.types import InlineKeyboardButton
+
+
+def song_markup(_, vidid):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["SG_B_2"],
+                callback_data=f"song_helper audio|{vidid}",
+            ),
+            InlineKeyboardButton(
+                text=_["SG_B_3"],
+                callback_data=f"song_helper video|{vidid}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🌻 sᴜᴩᴩᴏʀᴛ 🌻",
+                url=config.SUPPORT_GROUP,
+            ),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+        ],
+    ]
+    return buttons
