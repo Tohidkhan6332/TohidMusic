58433cf770014c5bd98ecd006e282e5b72afd32e
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-02-13 15:12:52 +0530

    TohidMusic

---
+++ TohidMusic/plugins/play/playmode.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2025 by Tohid_Help @ Github, < https://github.com/Tohidkhan6332 >
+# Subscribe On YT < Tohidkhan_6332 >. All rights reserved. © Tohid © Mr Tohid.
+
+"""
+Tohidkhan6332 is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Tohid <https://github.com/Tohidkhan6332>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import BANNED_USERS
+from strings import get_command
+from TohidMusic import app
+from TohidMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from TohidMusic.utils.decorators import language
+from TohidMusic.utils.inline.settings import playmode_users_markup
+
+### Commands
+PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
+
+
+@app.on_message(filters.command(PLAYMODE_COMMAND) & filters.group & ~BANNED_USERS)
+@language
+async def playmode_(client, message: Message, _):
+    playmode = await get_playmode(message.chat.id)
+    if playmode == "Direct":
+        Direct = True
+    else:
+        Direct = None
+    is_non_admin = await is_nonadmin_chat(message.chat.id)
+    if not is_non_admin:
+        Group = True
+    else:
+        Group = None
+    playty = await get_playtype(message.chat.id)
+    if playty == "Everyone":
+        Playtype = None
+    else:
+        Playtype = True
+    buttons = playmode_users_markup(_, Direct, Group, Playtype)
+    response = await message.reply_text(
+        _["playmode_1"].format(message.chat.title),
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
